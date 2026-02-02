a0676cf29ec83cda87035bad37fcb95b67f50a88
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2026-02-02 01:34:06 +0100

    chore: added swagger documentation for endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use validation types globally
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +14,19 @@ async function bootstrap() {
       transform: true,
     })
   )
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Taskflow API')
+    .setDescription('API documentation for Taskflow application')
+    .setTermsOfService('https://example.com/terms')
+    .setVersion('1.0')
+    .addServer('http://localhost:3001', 'Local server')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
