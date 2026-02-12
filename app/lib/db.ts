f9f53812b6e17d30ed4ccdbff077f2e0d73b89ee
paulDev <paulanselem35@gmail.com>
2026-02-12 23:55:01 +0100

    ui reset

--- app/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '../../generated/prisma/client';
-import { env } from "../lib/env";
-
-const connectionString = env.DATABASE_URL;
-
-export const adapter = new PrismaNeon({ connectionString });
-export const prisma = new PrismaClient({ adapter });
