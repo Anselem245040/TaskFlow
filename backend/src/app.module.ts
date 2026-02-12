9f4d769e96bf5edd5e533f9be11e06e68c70b663
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2026-02-12 22:21:34 +0100

    chore: scaffold room infrastructure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { ModuleModule } from './module/rooms/module.module';
+import { RoomModule } from './modules/rooms/room.module';
+import { RoomsService } from './modules/room/services/rooms.service';
 @Module({
-  imports: [CoreModule, AuthModule, UsersModule],
+  imports: [CoreModule, AuthModule, UsersModule, ModuleModule, RoomModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RoomsService],
 })
 export class AppModule {
 
