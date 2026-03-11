15bfdabfe8419cb358fce31007c4f1d73566c6d3
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2026-03-11 19:31:33 +0100

    feat:added validation for environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -32,6 +33,8 @@ async function bootstrap() {
 
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('port', 3000));
+  console.log(configService.get<string>('NODE_ENV'));
 }
 bootstrap();
